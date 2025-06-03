f32c3d4007244c5ae61d547d1cbe0c40f57bfa86
PleccyMM <pleccymm@outlook.com>
2025-06-03 19:49:25 +0100

    Login page changes and initial frontend, untested

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
+import './Login.css';
 
 function Login({ onLoginSuccess }) {
-    const [number, setNumber] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [staffNumber, setNumber] = useState('');
+    const [password, setLastName] = useState('');
 
     const handleNumberChange = (event) => {
         setNumber(event.target.value);
     };
 
-    const handleLastNameChange = (event) => {
+    const handlePasswordChange = (event) => {
         setLastName(event.target.value);
     };
 
@@ -16,8 +17,8 @@ function Login({ onLoginSuccess }) {
         event.preventDefault();
 
         const loginData = {
-            staff_number: number,
-            last_name: lastName
+            staff_number: staffNumber,
+            password: password
         };
 
         try {
@@ -42,29 +43,48 @@ function Login({ onLoginSuccess }) {
     };
 
     return (
-        <div className='Login'>
-            <h1>Fire Warden Tracker</h1>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label>Staff Number:</label>
-                    <input
-                        type='text'
-                        value={number}
-                        onChange={handleNumberChange}
-                        placeholder='Enter a Number'
+        <div className='login-page'>
+            <div className='login-left'>
+                <div className="image-container">
+                    <img
+                        src="/images/West-Downs.jpg"
+                        alt="Welcome illustration"
+                        className="left-image"
                     />
+                    <div className="left-content">
+                        <h1>Welcome Back</h1>
+                        <p className="login-text">Login to register your location, for wardens, or, for health staff, manage safety across campus</p>
+                    </div>
                 </div>
-                <div>
-                    <label>Last Name:</label>
-                    <input
-                        type="text"
-                        value={lastName}
-                        onChange={handleLastNameChange}
-                        placeholder="Enter Last Name"
-                    />
+            </div>
+            <div className='login-right'>
+                <div className='login-box'>
+                    <h2>Login</h2>
+                    <form>
+                        <label>Staff Number</label>
+                        <input
+                            type='text'
+                            value={staffNumber}
+                            onChange={handleNumberChange}
+                            required
+                        />
+                        <label>Password</label>
+                        <input 
+                            type='password' 
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required 
+                        />
+                        <button type='submit' onSubmit={handleLogin}>Sign In</button>
+                        <p className="register-text">
+                            Don't have an account?{' '}
+                            <a href="/register" className="register-link">
+                                Register
+                            </a>
+                        </p>
+                    </form>
                 </div>
-                <button type="submit">Submit</button>
-            </form>
+            </div>
         </div>
     );
 }
